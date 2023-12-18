a38826f76d13601cee40e6a44e8bd19909b2d4ea
Matti Pedersen <mrmattipants@gmail.com>
2023-12-18 03:20:27 -0600

    Update Set-NewDateOnUsrClassDat.ps1

--- Testing/Set-NewDateOnUsrClassDat.ps1
+++ Testing/Set-NewDateOnUsrClassDat.ps1
@@ -1,13 +1,27 @@
-﻿
+
 Remove-Variable * -ErrorAction SilentlyContinue; Remove-Module *; $error.Clear();
-
-$Username = "Username"
-
-$NewDate = Get-Date -Date "Tuesday, June 6, 2023 12:05:00 AM"
 
 $NumberofDays = 0
 
-If ($NewDate -and (!$NumberofDays -or $NumberofDays -lt 1)) {
+$Username = Read-Host "Enter Username"
+
+$Choice = Read-Host "Type `"1`" to Set Date/Time or Type `"2`" to Set Number of Days"
+
+If ($Choice -eq "1") {
+
+	$DateTime = Read-Host "Enter Date & Time (Tuesday, June 6, 2023 12:05:00 AM)"
+
+	$NewDate = Get-Date -Date $DateTime
+	
+} ElseIf ($Choice -eq "2") {
+
+	$DateTime = Read-Host "Enter the Number of Days (180)"
+	
+	$NumberofDays = $DateTime
+
+}
+
+If ($NumberofDays -eq 0 -and $NewDate -is [DateTime]) {
 
     $UsrClassDat = Get-Item "C:\Users\$($Username)\AppData\Local\Microsoft\Windows\UsrClass.dat" -force
 
@@ -19,25 +33,11 @@ If ($NewDate -and (!$NumberofDays -or $NumberofDays -lt 1)) {
 
     $($NewUsrClassDate)
 
-} ElseIf ($NumberofDays -and $NumberofDays -gt 0 -and !$NewDate) {
+} ElseIf ($NumberofDays -gt 0 -and $NewDate -isnot [DateTime]) {
 
     $UsrClassDat = Get-Item "C:\Users\$($Username)\AppData\Local\Microsoft\Windows\UsrClass.dat" -force
 
     $NewDate = ($UsrClassDat.LastWriteTime).AddDays(-$NumberofDays)
-
-    $UsrClassDat.LastWriteTime = $NewDate
-
-    $NewUsrClassDate = Get-Item "C:\Users\$($Username)\AppData\Local\Microsoft\Windows\UsrClass.dat" -force
-
-    Write-Host "`nThe `"LastWriteTime`" Attribute, from the `"UsrClass.dat`" File, which belongs to the `"$($Username)`" User Profile, has been Successfully Updated" -ForegroundColor Green
-
-    $($NewUsrClassDate)
-
-} ElseIf ($NumberofDays -and $NumberofDays -gt 0 -and $NewDate) {
-    
-    $UsrClassDat = Get-Item "C:\Users\$($Username)\AppData\Local\Microsoft\Windows\UsrClass.dat" -force
-
-    $NewDate = ($NewDate).AddDays(-$NumberofDays)
 
     $UsrClassDat.LastWriteTime = $NewDate
 
