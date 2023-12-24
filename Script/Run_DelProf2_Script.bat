7ec2fb2aa531e1cfff6612c41ed514b2f08cf489
Matti Pedersen <mrmattipants@gmail.com>
2023-12-23 16:29:43 -0800

    Update Run_DelProf2_Script.bat

--- Script/Run_DelProf2_Script.bat
+++ Script/Run_DelProf2_Script.bat
@@ -1,7 +1,7 @@
 @echo off
 
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File "%~dp0Update-NtUserDat.ps1"
+START /WAIT C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File "%~dp0Update-NtUserDat.ps1" | ECHO > nul
 
-"%~dp0DelProf2.exe" /d:90
+"%~dp0DelProf2.exe" /d:90 /q
 
 EXIT
