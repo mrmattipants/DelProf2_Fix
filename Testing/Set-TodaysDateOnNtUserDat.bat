d09cc66c0775be0534f4db9bc673ae234a513e00
Matti Pedersen <mrmattipants@gmail.com>
2024-04-12 15:05:30 -0500

    Delete Testing directory

--- Testing/Set-TodaysDateOnNtUserDat.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File "%~dp0Set-TodaysDateOnNtUserDat.ps1"
-
-PAUSE
--- Testing/Test_DelProf2_Script.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File "%~dp0Update-NtUserDat.ps1"
-
-"%~dp0DelProf2.exe" /d:90 /l
-
-PAUSE
